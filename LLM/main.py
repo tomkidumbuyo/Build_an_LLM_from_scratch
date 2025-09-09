15b3eb3d44da1257cc7ebc6035a0d0cde870fb64
thomas kidumbuyo <mryoungtommy@gmail.com>
2025-09-10 01:15:03 +0300

    Added attention implementation explanation
    We added a jupyter notebook to explain how we came up with multihead
    attention algorithm.

--- LLM/main.py
+++ LLM/main.py
@@ -44,28 +44,30 @@ else:
     with open(file_path, "r", encoding="utf-8") as file:
         raw_text = file.read()
 
+# 1. Text tokenization
 max_length = 4
 dataloader = create_dataloader_v1(raw_text, batch_size=2, max_length=max_length, stride=4, shuffle=False)
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 
+
 vocab_size = tokenizer.n_vocab
 output_dim = 256
-
 # torch.manual_seed(123)
 token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 # print(embedding_layer.weight)
 
+# 3. Creating token embeddings
 token_embeddings = token_embedding_layer(inputs)
 print(token_embeddings.shape)
 
+# 4. Encoding word positions in a sentence
 context_length = max_length
 pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
-i = torch.arange(context_length)
 pos_embeddings = pos_embedding_layer(torch.arange(context_length))
-print(pos_embeddings.shape)
 
 input_embeddings = token_embeddings + pos_embeddings
-print(input_embeddings.shape)
+
+
 
 
